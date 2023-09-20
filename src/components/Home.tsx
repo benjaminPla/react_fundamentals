76d9fd9fe0a84e8e3005f1b8a90b06c74969d835
benjaminPla <benjamin.pla@7-11.com>
2023-09-20 23:18:29 +0200

    delete Title component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,20 @@
 import React from "react";
-import Title from "./Title";
 
 const Home: React.FC = () => {
   return (
     <>
-      <Title text="React Fundamentals" />
+      <h1>React Fundamentals</h1>
+      <h2>🚧 Under Construction 🚧</h2>
+      <p>
+        This project is currently in development, and I'm actively working on
+        it.
+      </p>
+
+      <p>
+        This project is a demonstration of a highly scalable web application
+        built using React, Vite, TypeScript, and Jest. It covers all the
+        front-end fundamentals.
+      </p>
     </>
   );
 };
--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import Title from "./Title";
 
 const NotFound: React.FC = () => {
   return (
     <>
-      <Title text="404 - Not Found" />
+      <h1>404 - Not Found</h1>
       <p>The page you are looking for does not exist.</p>
     </>
   );
