3c63b03e65b04fdee47820f70ea642a4fce2e5c4
benjaminPla <benjamin.pla@7-11.com>
2023-09-21 18:35:21 +0200

    add Products page

---
+++ src/components/Products.tsx
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from "react";
+
+interface IProduct {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  image: string;
+}
+
+const Products: React.FC = () => {
+  const [products, setProducts] = useState<IProduct[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [expandedDescription, setExpandedDescription] = useState<number | null>(
+    null
+  );
+
+  const fetchData = async (url: string): Promise<IProduct[]> => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setProducts(data);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setLoading(false);
+    }
+  };
+
+  const toggleDescription = (productId: number) => {
+    if (expandedDescription === productId) {
+      setExpandedDescription(null);
+    } else {
+      setExpandedDescription(productId);
+    }
+  };
+
+  useEffect(() => {
+    const url = "https://fakestoreapi.com/products";
+    fetchData(url);
+  }, []);
+
+  return (
+    <div className="container mt-4">
+      <h1>Product List</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div className="row">
+          {products.map((product) => (
+            <div className="col-lg-3 col-md-4 col-sm-6 mb-4" key={product.id}>
+              <div className="card" style={{ height: "100%" }}>
+                <img
+                  src={product.image}
+                  alt={product.title}
+                  className="card-img-top img-fluid mt-4"
+                  style={{ maxHeight: "150px", objectFit: "contain" }}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{product.title}</h5>
+                  <p
+                    className="card-text"
+                    style={{
+                      maxHeight:
+                        expandedDescription === product.id ? "none" : "100px",
+                      overflow: "hidden",
+                      transition: "max-height 0.3s ease-out",
+                    }}
+                  >
+                    {product.description}
+                  </p>
+                  {product.description.length > 100 && (
+                    <button
+                      onClick={() => toggleDescription(product.id)}
+                      className="btn btn-link p-0"
+                    >
+                      {expandedDescription === product.id
+                        ? "Read Less"
+                        : "Read More"}
+                    </button>
+                  )}
+                  <p className="card-text">Price: ${product.price}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Products;
--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./components/Home";
 import NotFound from "./components/NotFound";
-// import Products from "./components/Products";
+import Products from "./components/Products";
 // import Cart from "./components/Cart";
 // import Checkout from "./components/Checkout";
 
 const router = createBrowserRouter([
   { path: "/", element: <Home /> },
+  { path: "/products", element: <Products /> },
   { path: "*", element: <NotFound /> },
 ]);
 
