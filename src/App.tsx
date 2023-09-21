8c41a13efe64a9d4cdb28baec17b4bd96d1774cc
benjaminPla <benjamin.pla@7-11.com>
2023-09-21 20:45:07 +0200

    add redux, modulize menuItems

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import React from "react";
+import { Provider } from "react-redux";
+import store from "./store";
+import { useSelector } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
+import { menuItems } from "./utils/menuItems";
 import Menu from "./components/Menu";
 import Footer from "./components/Footer";
 
 const App: React.FC = () => {
-  const menuItems = [
-    { title: "Home", link: "/" },
-    { title: "Products", link: "/products" },
-    { title: "Cart", link: "/cart" },
-    { title: "Checkout", link: "/checkout" },
-  ];
   return (
     <React.StrictMode>
-      <Menu items={menuItems} />
-      <RouterProvider router={router} />
-      <Footer />
+      <Provider store={store}>
+        <Menu items={menuItems} />
+        <RouterProvider router={router} />
+        <Footer />
+      </Provider>
     </React.StrictMode>
   );
 };
---
+++ src/utils/menuItems.ts
@@ -0,0 +1,12 @@
+interface IMenuItems {
+  title: string;
+  link: string;
+}
+[];
+
+export const menuItems: IMenuItems = [
+  { title: "Home", link: "/" },
+  { title: "Products", link: "/products" },
+  { title: "Cart", link: "/cart" },
+  { title: "Checkout", link: "/checkout" },
+];
