05621009172be3c56ed96fd7471f4eeb3b38d95c
benjaminPla <benjamin.pla@7-11.com>
2023-09-21 18:35:03 +0200

    improve NotFound page style

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -2,10 +2,15 @@ import React from "react";
 
 const NotFound: React.FC = () => {
   return (
-    <>
-      <h1>404 - Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
-    </>
+    <div
+      className="container d-flex justify-content-center align-items-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="text-center">
+        <h1>404 - Not Found</h1>
+        <p>The page you are looking for does not exist.</p>
+      </div>
+    </div>
   );
 };
 
