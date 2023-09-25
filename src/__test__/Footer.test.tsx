8beba99476e01703afea2b954ec324560d6e28c4
benjaminPla <benjamin.pla@7-11.com>
2023-09-25 16:24:39 +0200

    add Footer.test.tsx file

---
+++ src/__test__/Footer.test.tsx
@@ -0,0 +1,54 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import Footer from "../components/Footer";
+
+describe("Footer", () => {
+  beforeEach(() => {
+    render(<Footer />);
+  });
+
+  it("renders texts correctly", () => {
+    const footerText = `© ${new Date().getFullYear()} React Fundamentals`;
+
+    const text = screen.getByText(footerText);
+    const homeLink = screen.getByText("Home");
+    const aboutLink = screen.getByText("About");
+
+    expect(text).toBeInTheDocument();
+    expect(homeLink).toBeInTheDocument();
+    expect(aboutLink).toBeInTheDocument();
+  });
+
+  it("renders classNames correctly", () => {
+    const footer = screen.getByTestId("footer");
+    const container = screen.getByTestId("container");
+    const rows = screen.getAllByTestId("row");
+    const cols = screen.getAllByTestId("col");
+    const text = screen.getByTestId("text");
+    const ul = screen.getByTestId("ul");
+    const lis = screen.getAllByTestId("li");
+
+    expect(footer).toHaveClass("footer bg-light");
+    expect(container).toHaveClass("container");
+    rows.forEach((row) => {
+      expect(row).toHaveClass("row");
+    });
+    cols.forEach((col) => {
+      expect(col).toHaveClass("col");
+    });
+    expect(text).toHaveClass("text-center");
+    expect(ul).toHaveClass("list-unstyled d-flex justify-content-center");
+    lis.forEach((li) => {
+      expect(li).toHaveClass("mx-3");
+    });
+  });
+
+  it("links should redirect correctly", () => {
+    const homeLink = screen.getByText("Home");
+    const aboutLink = screen.getByText("About");
+
+    expect(homeLink).toHaveAttribute("href", "/");
+    expect(aboutLink).toHaveAttribute("href", "https://www.benjaminpla.com");
+    expect(aboutLink).toHaveAttribute("target", "_blank");
+  });
+});
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,22 +2,25 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="footer bg-light">
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <p className="text-center">
+    <footer className="footer bg-light" data-testid="footer">
+      <div className="container" data-testid="container">
+        <div className="row" data-testid="row">
+          <div className="col" data-testid="col">
+            <p className="text-center" data-testid="text">
               &copy; {new Date().getFullYear()} React Fundamentals
             </p>
           </div>
         </div>
-        <div className="row">
-          <div className="col">
-            <ul className="list-unstyled d-flex justify-content-center">
-              <li className="mx-3">
+        <div className="row" data-testid="row">
+          <div className="col" data-testid="col">
+            <ul
+              className="list-unstyled d-flex justify-content-center"
+              data-testid="ul"
+            >
+              <li className="mx-3" data-testid="li">
                 <a href="/">Home</a>
               </li>
-              <li className="mx-3">
+              <li className="mx-3" data-testid="li">
                 <a href="https://www.benjaminpla.com" target="_blank">
                   About
                 </a>
